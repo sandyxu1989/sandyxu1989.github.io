d25303b519d5820c55fd94adc45afd7e1ee48683
xushanshan <shanshan.xu@cm-dt.com>
2018-03-28 10:49:17 +0800

    express 框架

---
+++ public/js/index.js
@@ -0,0 +1,6 @@
+/**
+ * Created by 1 on 2018/3/27.
+ */
+const appJS = "这是javascript内的appJS";
+var dom = document.getElementById("app");
+dom.innerHTML = appJS;
---
+++ routes/test.js
@@ -0,0 +1,8 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET xuss page. */
+router.get('/', function(req, res, next) {
+    res.render('test', { title: 'test' });
+});
+module.exports = router;
